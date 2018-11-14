fae24f9fcca20f6ffd50071379c4cd95e794fc36
DamVan75 <damienvaneeckhout@gmail.com>
2018-11-14 18:40:05 +0100

    branche scrap ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_152349) do
+ActiveRecord::Schema.define(version: 2018_11_14_165751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "leads", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "persona"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscribers", force: :cascade do |t|
     t.string "email"
