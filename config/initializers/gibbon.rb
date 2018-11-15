3d4721cebb29b7bf2dcf4b8fb6c3e7ea2922595d
hrivierefr <legsmower@gmail.com>
2018-11-15 20:26:20 +0100

    Changement config pour credentials

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,6 +1,4 @@
-require('dotenv/load')
-
-Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY']
+Gibbon::Request.api_key = Rails.application.credentials.MAILCHIMP_API_KEY
 Gibbon::Request.timeout = 15
 Gibbon::Request.open_timeout = 15
 Gibbon::Request.symbolize_keys = true
